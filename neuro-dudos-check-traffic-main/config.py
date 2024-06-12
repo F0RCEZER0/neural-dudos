d94b9953bedf889e898a407246b6ce1bcfb19d67
Михаил Карпенко <mikhail.karpenko.2000@mail.ru>
2024-06-12 17:02:02 +0300

    config.py

--- neuro-dudos-check-traffic-main/config.py
+++ neuro-dudos-check-traffic-main/config.py
@@ -6,6 +6,3 @@ unique_labels = ['BENIGN', 'DDoS', 'PortScan', 'Bot', 'Infiltration',
 # model_path = './models/EIDM_8_4_classes.keras'
 model_path = './models/EIDM_8_4_classes.h5'
 dataset_path = "./data/web_attacks_balanced.csv"
-TOKEN = "7208546290:AAH70XKB2u1cDKOA45xSrxvHXZXOjUEIROg"
-
-ADMIS_ID = [479979542, 444766765]
